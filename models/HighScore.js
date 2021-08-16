04b72902bb9dae7ae96d25175b2a4de4fd98d929
Tom Virgo <tomcvirgo@gmail.com>
2021-08-16 10:26:16 +0100

    add HighSHighScore Model

---
+++ models/HighScore.js
@@ -0,0 +1,26 @@
+const { init } = require('../db/init');
+
+
+class HighScore {
+    constructor(name, category, difficulty, score){
+        this.name = name;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.score = score;
+    }
+
+    static create(name, category, difficulty, score){
+        return new Promise (async (resolve,reject) => {
+            try {
+                const db = await init();
+                await db.collection('highscores').insertOne({name, category, difficulty, score});
+                let newHighScore = new HighScore(name, category, difficulty, score);
+                resolve(newHighScore);
+            } catch (error){
+                reject(error);
+            }
+        })
+    }
+}
+
+module.exports = HighScore;
