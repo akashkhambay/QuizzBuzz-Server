3065131d1b02405832236cdee2a667e7b8e172a6
Tom Virgo <tomcvirgo@gmail.com>
2021-08-16 13:00:33 +0100

    update socket events

--- socketEvents/index.js
+++ socketEvents/index.js
@@ -1,6 +1,18 @@
 function initialise(socket){
     socket.emit('user connected')
     socket.on('disconnect', ()=>console.log('user disconnected'));
+    socket.on('create game', ({code, category, difficulty}) => {
+        console.log(`game created with the code ${code}`);
+        socket.join(code);
+    })
+    socket.on('join game', (code) => {
+        // check if the room exists first and if not send back an error message
+        console.log(`game joined with the code ${code}`);
+        socket.join(code);
+    })
+    socket.on('change gameState', (state) => {
+        
+    })
 }
 
 module.exports = { initialise };
